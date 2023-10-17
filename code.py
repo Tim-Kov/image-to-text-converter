bf1f30841e73ffa1d1e9cf12d433ba68b4a936a8
Tim-Kov <148255046+Tim-Kov@users.noreply.github.com>
2023-10-17 13:04:45 -0400

    code.py

--- code.py
+++ code.py
@@ -1,21 +1,23 @@
 from PIL import Image, ImageEnhance
 import random, os
 colorscale = [['r'],['r']],  [['r'],['t','i','c']],[['i','t','c'],['i','t','c']], [['i','t','c'],['f','v','F']],[['f','v','F'],['f','v','F']],[['f','v','F'], ['z','l']],[['z','l'],['z','l']],[['z','l'],['Y']],[['Y'],['Y']],[['Y'],['x','m']],[['x','m'],['x','m']],[['x','m'],['k','U']],[['k','U'],['k','U']],[['k','U'],['e','s','C','J']],[['e','s','C','J'],['e','s','C','J']],[['e','s','C','J'],['y','u','o','l','L','Z']],[['y','u','o','l','L','Z'],['y','u','o','l','L','Z']],[['y','u','o','l','L','Z'],['a','n','V']],[['a','n','V'],['a','n','V']],[['a','n','V'],['q','d','j','Q','S','X']],[['q','d','j','Q','S','X'],['q','d','j','Q','S','X']],[['q','d','j','Q','S','X'],['w','T','D','A']],[['w','T','D','A'],['w','T','D','A']],[['w','T','D','A'],['R']] ,[['R'],['R']],[['R'],['G','K','N']],[['G','K','N'],['G','K','N']],[['G','K','N'],['g','h','M']],[['g','h','M'],['g','h','M']],[['g','h','M'],['E','P']],[['E','P'],['E','P']],[['E','P'],['O','H']],[['O','H'],['O','H']],[['O','H'],['p']],[['p'],['p']],[['p'],['b']],[['b'],['b']],[['b'],['W']],[['W'],['W']],[['W'],['B']],[['B'],['B']]
-display = ''
-image = Image.open(os.path.join('uploadimage', os.listdir('uploadimage')[0]))
-enchancer = ImageEnhance.Color(image)
-enhanced = enchancer.enhance(2)
-enchancedImage = image.resize((image.size[0]//(image.size[0]//50),image.size[1]//(image.size[0]//50))).convert('L')
-pixels = list(enchancedImage.getdata())
-count = 0
-x=1
-while max(pixels)//x > 40:
-  x+=1
-for i in range(len(pixels)): 
-  display+=colorscale[round(pixels[i]//x)][0][0]
-  display+=colorscale[round(pixels[i]//x)][1][0]
-  count+=1
-  if count == image.size[0]//(image.size[0]//50):
-    display+='\n'
-    count = 0
-print(display)
+for j in range(len(os.listdir("uploadimage"))):
+  display = ''
+  image = Image.open(os.path.join('uploadimage', os.listdir('uploadimage')[j]))
+  enchancer = ImageEnhance.Color(image)
+  enhanced = enchancer.enhance(2)
+  enchancedImage = image.resize((image.size[0]//(image.size[0]//50),image.size[1]//(image.size[0]//50))).convert('L')
+  pixels = list(enchancedImage.getdata())
+  count = 0
+  x=1
+  while max(pixels)//x > 40:
+    x+=1
+  for i in range(len(pixels)): 
+    display+=colorscale[round(pixels[i]//x)][0][0]
+    display+=colorscale[round(pixels[i]//x)][1][0]
+    count+=1
+    if count == image.size[0]//(image.size[0]//50):
+      display+='\n'
+      count = 0
+  with open("result/result"+"image"+str(j)+'.txt', "w") as file:
+      file.write(display)
